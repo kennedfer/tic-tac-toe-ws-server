287de65312f582ea862764edbd8e85b2b31f05f8
kennedfer <kenned14santos@gmail.com>
2023-02-27 16:16:37 -0300

    testing

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const WebSocket = require("ws");
 let server = new WebSocket.Server({ port: 3000 });
 
+let users = {};
+
 server.on("connection", (ws) => {
   ws.send("cliente entrou...");
   console.log("cliente entrou...");
 
   ws.on("message", (message) => {
-    console.log(`Mensagem recebida: ${message}`);
+    const user = JSON.parse(message);
+
+    users[user.id] = user;
+    ws.send(users);
   });
 });
